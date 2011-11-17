a14051eccaa6a781b167dff61f9b58507203aebd
giantpune <giantpune@gmail.com>
2011-11-17 03:28:54 +0000

    * minor optimization - make LIMIT not evaluate stuff like "atoi(balbla+strlen(stuff))" 3 times

--- source/utils/tools.h
+++ source/utils/tools.h
@@ -2,7 +2,13 @@
 #define TOOLS_H_
 
 #define ABS(x) ( (x) >= (0) ? (x) : (-(x)) )
-#define LIMIT(x, min, max) ( ((x) < (min)) ? (min) : ((x) > (max)) ? (max) : (x) )
+#define LIMIT(x, min, max)																	\
+	({																						\
+		typeof( x ) _x = x;																	\
+		typeof( min ) _min = min;															\
+		typeof( max ) _max = max;															\
+		( ( ( _x ) < ( _min ) ) ? ( _min ) : ( ( _x ) > ( _max ) ) ? ( _max) : ( _x ) );	\
+	})
 #define ALIGN(x) (((x) + 3) & ~3)
 #define ALIGN32(x) (((x) + 31) & ~31)
 
