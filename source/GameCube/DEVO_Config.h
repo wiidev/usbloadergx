9c38fd50e2033443c1ac13cb21a4d43a8c6c77cc
Cyan <cyan06@users.noreply.github.com>
2012-07-16 16:11:26 +0000

    * I Forgot the added file in previous rev.

---
+++ source/GameCube/DEVO_Config.h
@@ -0,0 +1,36 @@
+/****************************************************************************
+ * Copyright (C) 2012 Dimok
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
+#ifndef DEVO_CONFIG_H_
+#define DEVO_CONFIG_H_
+
+#define LAUNCH_DEVO() ((void(*)(void))loader_bin)()
+
+#define DEVO_SIG				 0x3EF9DB23
+#define DEVO_VERSION			 0x00000100
+
+// Devolution
+typedef struct _DEVO_CFG
+{
+        u32 signature;                  //0x3EF9DB23
+        u16 version;                    //0x00000100
+        u16 device_signature;
+        u32 memcard_cluster;
+        u32 disc1_cluster;
+        u32 disc2_cluster;
+} DEVO_CGF;
+
+#endif
