6ab4fdd43619a218e7322e90b349d5ac4b950d9b
wiidev <wiidev@users.noreply.github.com>
2020-07-09 02:11:20 +0100

    Display Japanese descriptions correctly

--- source/banner/OpeningBNR.cpp
+++ source/banner/OpeningBNR.cpp
@@ -31,6 +31,7 @@ distribution.
 #include "usbloader/wbfs.h"
 #include "usbloader/wdvd.h"
 #include "usbloader/wbfs/wbfs_rw.h"
+#include "utils/sjis.h"
 #include "utils/uncompress.h"
 #include "themes/CTheme.h"
 #include "settings/GameTitles.h"
@@ -429,6 +430,7 @@ CustomBanner *OpeningBNR::CreateGCBanner(const discHdr * header)
 			}
 		}
 
+		// sets the developer
 		wString str;
 		str.resize(strlen((char *) openingBnr->description[language].developer));
 		for(u32 i = 0; i < str.size(); i++)
@@ -437,11 +439,20 @@ CustomBanner *OpeningBNR::CreateGCBanner(const discHdr * header)
 		banner->SetBannerText("T_Coded_by", tr("Developer:"));
 		banner->SetBannerText("T_coder", str.toUTF8().c_str());
 
-		str.resize(strlen((char *) openingBnr->description[language].long_description));
-		for(u32 i = 0; i < str.size(); i++)
-			str[i] = *(openingBnr->description[language].long_description + i);
+		// sets the description and converts encodings (Japan and Taiwan)
+		if(header->id[3] == 'J' || header->id[3] == 'W')
+		{
+			string description((char *) openingBnr->description[language].long_description);
+			banner->SetBannerText("T_short_descript", sj2utf8(description).c_str());
+		}
+		else
+		{
+			str.resize(strlen((char *) openingBnr->description[language].long_description));
+			for(u32 i = 0; i < str.size(); i++)
+				str[i] = *(openingBnr->description[language].long_description + i);
 
-		banner->SetBannerText("T_short_descript", str.toUTF8().c_str());
+			banner->SetBannerText("T_short_descript", str.toUTF8().c_str());
+		}
 
 		// free buffer
 		free(openingBnr);
